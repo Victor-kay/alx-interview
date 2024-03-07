4cc8c383b3cc4134b91ebae6c791838e2b9411d6
Victor-kay <jvictorkingorikay94@gmail.com>
2024-03-07 17:07:55 +0000

    correction

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,19 @@
+#!/usr/bin/env python3
+"""
+This script defines a function pascal_triangle(n) that returns Pascal's triangle up to the specified number of rows 'n' as a list of lists of integers.
+Returns an empty list if n <= 0.
+"""
+
 def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the specified number of rows 'n'.
+    
+    Args:
+        n (int): Number of rows in the Pascal's triangle.
+        
+    Returns:
+        list: List of lists representing Pascal's triangle.
+    """
     if n <= 0:
         return []
 
