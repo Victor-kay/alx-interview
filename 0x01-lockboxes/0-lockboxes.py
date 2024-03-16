5ee3dc7eeb1dda05ced947683c09176c4c9a38f4
Victor-kay <jvictorkingorikay94@gmail.com>
2024-03-16 09:43:23 +0000

    merge 2

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,7 +4,7 @@ Determines if all the boxes can be opened.
 """
 
 
-def canUnlockAll(boxes):
+def can_unlock_all(boxes):
     """
     Determines if all the boxes can be opened.
 
@@ -35,10 +35,10 @@ def canUnlockAll(boxes):
 # Test cases
 if __name__ == "__main__":
     boxes1 = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes1))  # Output: True
+    print(can_unlock_all(boxes1))  # Output: True
 
     boxes2 = [[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]
-    print(canUnlockAll(boxes2))  # Output: True
+    print(can_unlock_all(boxes2))  # Output: True
 
     boxes3 = [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes3))  # Output: False
+    print(can_unlock_all(boxes3))  # Output: False
